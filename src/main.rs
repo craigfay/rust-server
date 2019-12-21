1bce1d7b28fed0d8566c344edb2beacdb192f5c2
craig fay <craigfay1@gmail.com>
2019-12-21 11:30:49 -0700

    Serve HTML

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::net::TcpStream;
 use std::net::TcpListener;
 use std::io::{Read, Write};
+use std::fs;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -14,11 +15,13 @@ fn main() {
 
 
 fn handle_connection(mut stream: TcpStream) {
+    println!("connection");
     let mut buffer = [0; 512];
 
     stream.read(&mut buffer).unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    let html = fs::read_to_string("index.html").unwrap();
+    let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", html);
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
