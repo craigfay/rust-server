e3d666d9a5d7e62fc2924595c1fa85281e49d19b
craig fay <craigfay1@gmail.com>
2019-12-22 14:09:08 -0700

    Refactor

--- src/main.rs
+++ src/main.rs
@@ -20,13 +20,17 @@ fn handle_connection(mut stream: TcpStream) {
     let get = b"GET / HTTP/1.1\r\n";
 
     if buffer.starts_with(get) {
+        let status_line = "HTTP/1.1 200 OK\r\n\r\n";
         let contents = fs::read_to_string("index.html").unwrap();
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
+
+        let response = format!("{}{}", status_line, contents);
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     } else {
+        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
         let contents = fs::read_to_string("404.html").unwrap();
-        let response = format!("HTTP/1.1 404 Not Found\r\n\r\n{}", contents);
+    
+        let response = format!("{}{}", status_line, contents);
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     }
