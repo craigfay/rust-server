743fd7f124cb9eb17db934e90c810d10b6d5226b
craig fay <craigfay1@gmail.com>
2019-12-23 09:40:11 -0700

    Add docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,15 @@
 pub struct ThreadPool;
 
 impl ThreadPool {
+    /// Create a new thread pool
+    ///
+    /// The size parameter represents the number of threads in the pool.
+    ///
+    /// # Panics
+    ///
+    /// The `new` function will panic if the size is 0.
     pub fn new(size: usize) -> ThreadPool {
+        assert!(size > 0);
         ThreadPool
     }
 }
